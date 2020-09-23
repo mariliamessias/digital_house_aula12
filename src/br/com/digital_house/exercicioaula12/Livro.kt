76cb3a601cf24c872beaec822a00fdb4c6855a0a
mariliamessias <marilia.messias@mercadolivre.com>
2020-09-23 20:20:09 -0300

    adição do método toString na classe livro

--- src/br/com/digital_house/exercicioaula12/Livro.kt
+++ src/br/com/digital_house/exercicioaula12/Livro.kt
@@ -1,3 +1,23 @@
 package br.com.digital_house.exercicioaula12
 
-class Livro (var codigo: String, var titulo: String, var autor: String, var anoLancamento : Int, var quantidade: Int, var preco: Double)
+class Livro {
+    var codigo: String
+    var titulo: String
+    var autor: String
+    var anoLancamento : Int
+    var quantidade: Int
+    var preco: Double
+
+    constructor(codigo: String, titulo: String, autor: String, anoLancamento: Int, quantidade: Int, preco: Double) {
+        this.codigo = codigo
+        this.titulo = titulo
+        this.autor = autor
+        this.anoLancamento = anoLancamento
+        this.quantidade = quantidade
+        this.preco = preco
+    }
+
+    override fun toString(): String {
+        return "Livro(codigo='$codigo', titulo='$titulo', autor='$autor', anoLancamento=$anoLancamento, quantidade=$quantidade, preco=$preco)"
+    }
+}
